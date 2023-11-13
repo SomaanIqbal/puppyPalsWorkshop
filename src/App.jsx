b68d46b9d972790e7b4ec2c33600b5be60ea9c0d
SomaanIqbal <isomaan@gmail.com>
2023-11-13 12:40:14 -0800

    Puppy Data with useState tab complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react'
+import { puppyList } from './data'
 
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(puppies);
 
   return (
     <>
       <div>
-      
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+        }
       </div>
-      
+
     </>
-  )
+  );
 }
 
 export default App
