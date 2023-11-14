d5589874959dba3b4d6af6914b3c66f69e939e23
SomaanIqbal <isomaan@gmail.com>
2023-11-13 21:58:39 -0800

    Applied css to the application

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,40 @@ import { puppyList } from './data'
 
 import './App.css'
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null);
   const featuredPup = puppies.find((pup) => pup.id === featPupId)
-  
-  console.log(puppies);
-  
+
+  console.log(featPupId)
+  // console.log(puppies);
+
   return (
     <>
-      <div>
+      <div className='app'>
         {featPupId && (
-          <div>
+          <div className='featuredPupBox'>
             <h2>{featuredPup.name}</h2>
             <ul>
               <li>Age: {featuredPup.age}</li>
               <li>Email: {featuredPup.email}</li>
             </ul>
           </div>
+
         )}
+        <div>
 
-        {puppies.map((puppy) => {
-          return <p onClick={() => {
-            
-            console.log(featuredPup);
+          {puppies.map((puppy) => {
+            return <p onClick={() => {
 
-            return setFeatPupId(puppy.id)
-          }} key={puppy.id}>{puppy.name}</p>
-        })
-        }
+              // console.log(featuredPup);
+
+              return setFeatPupId(puppy.id)
+            }} key={puppy.id}>{puppy.name}</p>
+          })
+          }
+        </div>
 
 
 
